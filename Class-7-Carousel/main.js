df3291227ac93cdfbcb1fd66cd7bf81cf4983432
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-22 10:03:10 +0600

    Add files via upload

---
+++ Class-7-Carousel/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+ 
+  $('.owl-carousel').owlCarousel({
+    margin: 30,
+    items: 4,
+    autoplay: false,
+    loop: true,
+    nav: true,
+    navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"]
+  })
+ 
+});
+
