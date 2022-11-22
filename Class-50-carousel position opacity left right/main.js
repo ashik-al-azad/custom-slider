778c8e9b90443504aef52585befd022462ea078c
Ashik Al Azad <83364890+ashik-al-azad@users.noreply.github.com>
2022-11-22 10:10:49 +0600

    Add files via upload

---
+++ Class-50-carousel position opacity left right/main.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+ 
+  $('.owl-carousel').owlCarousel({
+    items: 3,
+    nav: true,
+    dots: true,
+    loop: true,
+    autoplay: false,
+    margin: 30
+  });
+ 
+});
+
